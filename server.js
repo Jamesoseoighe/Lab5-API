ea6b8ef094693bb3c36dce965496aaa32868125f
James Joyce <jamesoseoighe@gmail.com>
2024-10-25 13:29:57 +0000

    Movies exercise 5

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Welcome to Data Respresentation & Querying');
 });
+
+
 
 
 app.get('/hello/:name', (req, res) => {
@@ -21,4 +23,32 @@ app.listen(port, () => {
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
+});
+
+
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    res.json({ movies });
 });
